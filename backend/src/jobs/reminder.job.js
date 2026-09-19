60f07a1ed3234f5116dd4373f9e85fa7d31dae7d
Sourav Paul <sourav97972@gmail.com>
2026-09-19 20:33:22 +0530

    fix: set cron job run every hour

--- backend/src/jobs/reminder.job.js
+++ backend/src/jobs/reminder.job.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import { processDueReminders } from '../services/reminder.service.js'
 
 export const startReminderCron = () => {
-    cron.schedule('* * * * *', async () => {
+    cron.schedule('0 * * * *', async () => { //hourly
         console.log('[reminder cron] running at', new Date().toISOString())
         await processDueReminders()
     })
